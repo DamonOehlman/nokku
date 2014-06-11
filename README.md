c2e7c68699b473907a50bf6cd36b52af7239f34b
Damon Oehlman <damon.oehlman@gmail.com>
2014-06-12 09:03:41 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# nokku
+
+This is a node application container that is similar to and inspired by
+[dokku](https://github.com/progrium/dokku). The primary rationale for
+creating nokku is that I had a couple of OpenVZ containers available to
+me, which is an environment in which [docker](http://docker.io) cannot
+be used (AFAIK).  So nokku was born.
+
+## Installation
+
+ERROR: could not find: 
---
+++ index.js
@@ -0,0 +1,13 @@
+/**
+  # nokku
+
+  This is a node application container that is similar to and inspired by
+  [dokku](https://github.com/progrium/dokku). The primary rationale for
+  creating nokku is that I had a couple of OpenVZ containers available to
+  me, which is an environment in which [docker](http://docker.io) cannot
+  be used (AFAIK).  So nokku was born.
+
+  ## Installation
+
+  <<< docs/installation.md
+**/
