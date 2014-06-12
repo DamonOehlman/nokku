ed395c28299fd5dee6b13a91166052a1967902c6
Damon Oehlman <damon.oehlman@gmail.com>
2014-06-12 14:01:22 +1000

    Installing node and nginx if not installed

--- nokku-bootstrap.sh
+++ nokku-bootstrap.sh
@@ -6,10 +6,28 @@ set -e
 set -o errtrace
 
 # detect the osversion
-OS_VERSION=$(lsb_release --release | sed -E s/[^0-9\.]//g)
+OS_VERSION=`lsb_release -sr`
+INSTALL="apt-get install -y"
+
+function installNode() {
+  apt-add-repository -y ppa:chris-lea/node.js
+  apt-get update
+  $INSTALL nodejs
+}
+
+function installNginx() {
+  apt-add-repository -y ppa:nginx/stable
+  apt-get update
+  $INSTALL nginx
+}
 
 # ensure we have up-to-date packages
 apt-get update
 
-# if we don't have the `add-apt-repository` command install it now
-hash apt-add-repository 2> /dev/null || apt-get -y install python-software-properties
+# install required tools
+hash apt-add-repository 2> /dev/null || $INSTALL python-software-properties
+hash git 2> /dev/null || $INSTALL git
+
+# install node and nginx
+hash node 2> /dev/null || installNode
+hash nginx 2> /dev/null || installNginx
